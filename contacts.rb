f7f53e6468461937b8b023b0c98cdd1af36a97af
BenEdick <brge85@gmail.com>
2013-08-20 20:53:09 -0700

    age addition to person class

--- contacts.rb
+++ contacts.rb
@@ -4,6 +4,7 @@ class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :phone
+  attr_accessor :age
 
   def full_name
     first_name + " " + last_name
@@ -18,10 +19,10 @@ class Contacts
       command = ask("> ")
 
       if command == "list"
-        puts "Name".ljust(30) + "Phone".rjust(12)
+        puts "Name".ljust(30) + "Phone".ljust(50) + "Age".rjust(12)
         puts "-" * 42
         people.each do |person|
-          puts person.full_name.ljust(30) + person.phone.rjust(12)
+          puts person.full_name.ljust(30) + person.phone.ljust(50) + person.age.rjust(12)
         end
 
       elsif command == "add"
@@ -30,6 +31,7 @@ class Contacts
         person.first_name = ask("First Name: ")
         person.last_name  = ask("Last Name: ")
         person.phone      = ask("Phone: ")
+        person.age      = ask("Age: ")
 
         people.push(person)
       elsif command == "quit"
